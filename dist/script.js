4df2b3169e6670c105416576a26d595bcfe8cc74
Sergio <sergio@Sergios-iMac.local>
2022-06-14 10:56:31 -0500

    add functions

--- dist/script.js
+++ dist/script.js
@@ -3,14 +3,25 @@
 // let timeLine = gsap.timeline({ repeat: 1, yoyo: true })
 
 let timeLine = gsap.timeline({ defaults: { opacity: 0, ease: 'back' } })
-timeLine
-  .from('#demo', { ease: 'linear' })
-  .from('h1', { x: 80 })
-  .from('h2', { x: -80 })
-  .from('p', { y: 30 })
-  .from('button', { y: 30 })
-  .from('#items >g', {
-    scale: 0,
-    stagger: 0.4,
-    transformOrigin: '50% 50%',
-  })
+
+function init() {
+  timeLine
+    .from('#demo', { ease: 'linear', autoAlpha: 0 })
+    .from('h1', { x: 80, duration: 1 })
+    .from('h2', { x: -80, duration: 1 }, '<')
+    .from('p', { y: 30 }, '-=0.2')
+    .from('button', { y: 30 }, '-=0.4')
+    .from(
+      '#items >g',
+      {
+        scale: 0,
+        stagger: 0.4,
+        transformOrigin: '50% 50%',
+      },
+      '-=0.2'
+    )
+}
+
+window.addEventListener('load', function () {
+  init()
+})
